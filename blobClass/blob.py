07249b1cd63dbc06f0ba0fe0cf8619b9592dd845
Samuel Burck <sburck11@gmail.com>
2015-09-27 14:20:21 -0400

    coding

--- blobClass/blob.py
+++ blobClass/blob.py
@@ -1,37 +1,69 @@
+from PIL import Image
+import numpy as np
+import random
 ### Blob
 
-# pick starting pt on binary plot
-# 
-# Propagate:
-	# get next pixel
-	# check for hole on 1/0 plot using sweep line algo.
-	# pick rand greater than blobThresh, less than surrounding blob +- innerThresh, color pixel
+# typePlot:
+# 	get random point
+# 	pick random point with probability of being close to last picked
+# 	Keep a list of neighboring points, most recently added pushed at end.
 
-# fill outer:
-	 pick random
-	 
+# Implement hole checking algo later
 
-# 
+# imgPlot:
+# 	for blob:
+# 		Pick random blob pt.
+# 		pick random color #
+# 		add neighboring points to array
+# 		pick pt from array based on distribution
+# 		add neighbors to array
+# 		pick color based on distribution
+
+# 	for bg:
+# 		pick random point
+# 		add neighbors
+# 		get neighbor color
+
+# getNeighbors:
+# 	return list of neighboring cells
+
+# getNeighbor color:
+# 	return color of neighbor with prob dist. based on darkest/lightest
+
+# get max color:
+# 	get color limit based on dist. to blob and max thresh
+
 
 class Blob():
 
 	def __init__(self, numBlob, minSize, maxSize, outerThresh, blobThresh, innerThresh):
-		self.binPlot=None
-		self.lastPlot=None
-		self.imgPlot=None
+		self.binType=np.zeros((100, 100), dtype = numpy.int)
+		self.imgPlot=np.zeros((100, 100), dtype = numpy.int)
 		self.numBlob=numBlob
 		self.minSize=minSize
 		self.maxSize=maxSize
 		self.outerThresh=outerThresh
 		self.blobThresh=blobThresh
 		self.innerThresh=innerThresh
-		self.lastPix=None
+		self.outerPixList=None
+		self.innerPixList=None
+		self.pix=None
+		# 0 = filling binType, 1 = filling blob, 2 = filling background
+		self.stage=0
+
+
 
 # Take coordinates of a pixel, return probability of pixel inclusion based on last pixels assigned.
 	def getPix(self):
-		if(self.lastPix=None):
-			return #(random)
-		neighbors=
+		if(self.pix=None):
+			a=random.randint(0, 255)
+			b=random.randint(0, 255)
+			self.typePlot[a][b]=2
+			self.pix=(a, b)
+			return (a, b)
+		if(self.stage=True)
+			x=random.gauss(0, len(self.))
+
 
 	def pixProb(self, a, b):
 		remProb = #random
--- randScript.py
+++ randScript.py
@@ -1,8 +1,22 @@
 import random
+import numpy as np
 
+# mu = 125
+# sigma = 25
 
-mu = 125
-sigma = 20
+# for i in range(20):
+# 	x=random.gauss(mu, sigma)
+# 	if (x>mu):
+# 		x=125-(x-125)
+# 	if(x<0):
+# 		x=0
+# 	print x
+# array = np.zeros((100, 100), dtype = np.int)
+# print array
+# array[0][0] = 1
+# print array
 
-for i in range(20):
-	print random.gauss(mu, sigma)
+a = random.randint(0, 255)
+b = random.randint(0, 255)
+print a
+print b
